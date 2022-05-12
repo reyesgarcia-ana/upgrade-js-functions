2892671a8d8af9b3c2ebc1164fcd747c960c7230
reyesgarcia-ana <reyesgarzia.ana@gmail.com>
2022-05-12 20:01:09 +0200

    iteration#3

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
 window.alert("Probando javaScript");
+
+
+// Iteration#1 -----------------------
+// Completa la función que tomando dos números como argumento devuelva el más alto
+
+function sum(numberOne , numberTwo) {
+  
+    if ((numberOne-numberTwo) < 0) {console.log(numberTwo, "es el mayor");}
+    if ((numberOne-numberTwo) > 0) {console.log(numberOne, "es el mayor");}
+    return;
+  };
+
+  let a = prompt ("Introduce el primer número: ");
+  let b = prompt ("Introduce el segundo número: ");
+  sum (a,b);
+
+// Iteration#2: Buscar la palabra más larga ----------------------------
+// Completa la función que tomando un array de strings como argumento devuelva el más 
+//largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Ejemplogrande2', 'Spiderman', 'Captain M.'];
+
+function findLongestWord(param) {
+  
+  let maxLength=0;
+  let longest;
+
+  for (i=0; i<avengers.length; i++) {
+  
+    let element = avengers[i];
+
+    if (element.length > maxLength) {
+      longest = avengers[i];
+      maxLength = longest.length;
+    }
+
+  };
+
+  console.log(`El elemento más largo es: ${longest}`);
+}
+
+findLongestWord(avengers);
+
+// Iteration#3: Calcular la suma ----------------------------
+// Implemente la función denominada sumNumbers que toma un 
+// array de números como argumento y devuelve la suma de todos los números de la matriz.
+
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(param) {
+  
+  let total = 0;
+
+  for (i=0; i<(param.length); i++) {
+    
+    sumNumbers = param[i] + total;
+    total = sumNumbers;
+
+  }
+
+  console.log(total);
+  console.log(param.length);
+}
+
+sumAll(numbers);
